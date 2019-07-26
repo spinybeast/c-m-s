317d39a8cb77527c9e8ecf5902e4796f21d27b8e
spiny_beast <spiny_beast@mail.ru>
2019-07-26 15:59:13 +0300

    fix footer

--- resources/assets/js/components/Footer.js
+++ resources/assets/js/components/Footer.js
@@ -5,15 +5,15 @@ export const Footer = () => (
         <div className="container d-flex flex-row justify-content-between align-items-center">
             <div className="d-flex flex-row copyright">{new Date().getFullYear()} &copy; Cyclone Music Space<span className="d-none d-sm-inline">. All rights reserved.</span></div>
             <div className="d-flex flex-row">
-                <a className="btn btn-social-icon btn-vk" target="_blank" title="Мы ВКонтакте"
+                <a className="btn btn-social-icon btn-vk" target="_blank" title="VK"
                    href="https://vk.com/cyclone_music_space">
                     <i className="fa fa-vk"></i>
                 </a>
-                <a className="btn btn-social-icon btn-facebook" target="_blank" title="Мы на Facebook"
-                   href="https://www.facebook.com/anton.brezhnev.58">
+                <a className="btn btn-social-icon btn-facebook" target="_blank" title="Facebook"
+                   href="https://www.facebook.com/cyclone_music_space-720106248448490/">
                     <i className="fa fa-facebook"></i>
                 </a>
-                <a className="btn btn-social-icon btn-soundcloud" target="_blank" title="Мы на Soundcloud"
+                <a className="btn btn-social-icon btn-soundcloud" target="_blank" title="Soundcloud"
                    href="https://soundcloud.com/tony-cyclonez">
                     <i className="fa fa-soundcloud"></i>
                 </a>
